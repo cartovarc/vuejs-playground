a49badc200646420c26390612b2d7ce45d09b10e
Carlos Tovar <cartovarc@gmail.com>
2020-03-24 13:41:52 -0500

    quasarframework: section 4
    delete task does not work (course bug), trigger events in other components will be covered later

---
+++ quasarframework/awesome-todo/src/components/Task.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <div><slot></slot> {{ index }}</div>
+    <small>{{ task.dueDate}} @ {{ task.dueTime }}</small>
+    <button @click="deleteTask(index)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task', 'index']
+}
+</script>
+
+<style>
+</style>
--- quasarframework/awesome-todo/src/pages/Index.vue
+++ quasarframework/awesome-todo/src/pages/Index.vue
@@ -1,34 +1,12 @@
 <template>
   <q-page padding>
-    <button @click="counter++" style="position: absolute; right: 10px;">{{ counter }}</button>
-
-    <input
-      v-model="message"
-      @keyup.esc="clearMessage"
-      @keyup.enter="alertMessage"
-      @mouseleave="clearMessage"
-      v-autofocus
-      :style="errorStyle" />
-    <button @click="clearMessage">Clear</button>
-
-    <div v-bind:class="{ 'warning' : message.length > 22 }">{{message.length}}</div>
-
-    <!-- v-show, hide with style. -->
-    <h5
-      v-show="message.length"
-      class="border-gray">{{message}}</h5>
-    <!--  v-if, delete from DOM. -->
-    <h5
-      v-if="message.length"
-      class="border-gray">{{message}}</h5>
-
-    <h6 v-else>No message entered :(</h6>
-
-    <hr>
-
-    <p :class="{'my-align-1': message.length > 22, 'my-align-2': message.length <= 22}" ref="upperCaseMessage" >Uppercase message: {{ messageUpperCase }}</p>
-    <p>Lowercase message: {{ message | messageLowerCase }}</p>
-
+    <ul>
+      <task v-for="(task, index) in tasks"
+      :key="task.id"
+      :task="task"
+      :index="index"> {{ task.name }}
+      </task>
+    </ul>
   </q-page>
 </template>
 
@@ -36,87 +14,38 @@
 export default {
   data() {
     return {
-      message: "I love Vue.js! so hard",
-      counter: 0,
-    }
-  },
-  // if we declare messageUpperCase as method, "messageUpperCase was fired" will printed for each clic in counter
-  computed: {
-    messageUpperCase() {
-      console.log("messageUpperCase was fired");
-      return this.message.toUpperCase();
-    },
-    errorStyle() {
-      if(this.message.length > 22){
-        return {
-          'color': 'red',
-          'background': 'pink'
+      tasks: [
+        {
+          id: 1,
+          name: "Go to shop",
+          dueDate: "2020/04/23",
+          dueTime: "18:30"
+        },
+        {
+          id: 2,
+          name: "Get bananas",
+          dueDate: "2020/04/23",
+          dueTime: "18:40"
+        },
+        {
+          id: 3,
+          name: "Get apples",
+          dueDate: "2020/04/23",
+          dueTime: "18:50"
         }
-      }else{
-        return {}
-      }
+      ]
     }
   },
   methods: {
-    clearMessage() {
-      this.message = '';
-    },
-    alertMessage() {
-      alert(this.message);
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
     }
   },
-  filters: {
-    messageLowerCase(value) {
-      return value.toLowerCase();
-    }
-  },
-  directives: {
-    autofocus: {
-      inserted(el) {
-        el.focus();
-      }
-    }
-  },
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  created() {
-    console.log("created");
-  },
-  beforeMount() {
-    console.log("beforeMount");
-  },
-  mounted() {
-    console.log("mounted");
-    this.$refs.upperCaseMessage.value = '123456';
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-   updated() {
-    console.log("updated");
-  },
-  beforeDestroy() {
-    console.log("beforeDestroy");
-  },
-  destroyed() {
-    console.log("destroyed");
-  },
+  components: {
+    'task': require('components/Task.vue').default
+  }
 }
 </script>
 
 <style>
-  .border-gray {
-    border: 1px solid gray;
-  }
-  .warning {
-    color: yellow;
-    background: black;
-  }
-  .my-align-1 {
-    text-align: center;
-  }
-  .my-align-2 {
-    text-align: right;
-  }
 </style>
