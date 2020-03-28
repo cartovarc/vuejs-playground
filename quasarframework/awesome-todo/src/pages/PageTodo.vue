e62c2dd9ea1fe6d377a73cd8b1051c1d067ee264
Carlos Tovar <cartovarc@gmail.com>
2020-03-28 00:00:18 -0500

    quasarframework: section 9

--- quasarframework/awesome-todo/src/pages/PageTodo.vue
+++ quasarframework/awesome-todo/src/pages/PageTodo.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="q-pa-md">
     <q-list
+      v-if="Object.keys(tasks).length"
       bordered
       separated>
       <task
@@ -10,6 +11,20 @@
         :id="key">
       </task>
     </q-list>
+
+    <div class="absolute-bottom text-center q-mb-lg">
+      <q-btn
+      @click="showAddTask = true"
+        round
+        color="primary"
+        size="24px"
+        icon="add"
+      />
+    </div>
+
+    <q-dialog v-model="showAddTask">
+      <add-task @close="showAddTask = false"/>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -18,11 +33,17 @@
 import { mapGetters } from 'vuex'
 
 export default {
+  data() {
+    return {
+      showAddTask: false
+    }
+  },
   computed: {
     ...mapGetters('tasks', ['tasks']),
   },
   components: {
-    'task': require('components/Task/Task.vue').default
+    'task': require('components/Task/Task.vue').default,
+    'add-task': require('components/Modals/AddTask.vue').default,
   }
 }
 </script>
--- quasarframework/awesome-todo/src/store/store-tasks.js
+++ quasarframework/awesome-todo/src/store/store-tasks.js
@@ -1,25 +1,26 @@
 import Vue from 'vue';
+import { uid } from 'quasar'
 
 const state = {
   tasks: {
-    "ID1": {
-      name: 'Get chicharron',
-      completed: false,
-      dueDate: '2020/03/27',
-      dueTime: '15:30'
-    },
-    "ID2": {
-      name: 'Get bananas',
-      completed: true,
-      dueDate: '2020/03/28',
-      dueTime: '16:30'
-    },
-    "ID3": {
-      name: 'Get apples',
-      completed: false,
-      dueDate: '2020/03/29',
-      dueTime: '17:30'
-    }
+    // "ID1": {
+    //   name: 'Get chicharron',
+    //   completed: false,
+    //   dueDate: '2020/03/27',
+    //   dueTime: '15:30'
+    // },
+    // "ID2": {
+    //   name: 'Get bananas',
+    //   completed: true,
+    //   dueDate: '2020/03/28',
+    //   dueTime: '16:30'
+    // },
+    // "ID3": {
+    //   name: 'Get apples',
+    //   completed: false,
+    //   dueDate: '2020/03/29',
+    //   dueTime: '17:30'
+    // }
   }
 }
 
@@ -27,8 +28,11 @@ const mutations = {
   updateTask(state, payload) {
     Object.assign(state.tasks[payload.id], payload.updates);
   },
-  deleteTask(state, id){
+  deleteTask(state, id) {
     Vue.delete(state.tasks, id);
+  },
+  addTask(state, payload) {
+    Vue.set(state.tasks, payload.id, payload.task);
   }
 }
 
@@ -38,6 +42,14 @@ const actions = {
   },
   deleteTask({ commit }, id){
     commit('deleteTask', id);
+  },
+  addTask({ commit }, task) {
+    let id = uid();
+    let payload = {
+      id: id,
+      task: task
+    }
+    commit('addTask', payload);
   }
 }
 
