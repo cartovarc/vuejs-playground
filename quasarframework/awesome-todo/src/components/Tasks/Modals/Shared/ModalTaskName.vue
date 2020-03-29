a6facd10b2652f8ee8a80ef8e24426957346d168
Carlos Tovar <cartovarc@gmail.com>
2020-03-29 13:10:32 -0500

    quasarframework: checkpoint for issue in section 14

--- quasarframework/awesome-todo/src/components/Tasks/Modals/Shared/ModalTaskName.vue
+++ quasarframework/awesome-todo/src/components/Tasks/Modals/Shared/ModalTaskName.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="q-mb-sm">
     <q-input
+      outlined
       :value="name"
       @input="$emit('update:name', $event)"
+      :rules="[val => !!val || 'Field is required']"
       autofocus
+      v-select-all
       clearable
       clear-icon="close"
-      label="Task name"
-      outlined
       ref="taskName"
-      :rules="[val => !!val || 'Field is required']"/>
+      label="Task name"/>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['name']
+  props: ['name'],
+  directives: {
+    selectAll: {
+      inserted(el) {
+        // issue, select does not work in first popup
+        // https://github.com/cartovarc/vuejs-playground/issues/7
+        let input = el.querySelector('.q-field__native')
+        input.addEventListener('focus', () => {
+          if(input.value.length) {
+            input.select()
+          }
+        });
+      }
+    }
+  }
 }
 </script>
